2852292697222b629e6ba2565b02c3d42079fa37
AradProgramming <aradprogramming@gmail.com>
2026-09-24 12:12:26 +0330

    Add Orbit download progress events

--- adland-vertex/electron/preload.cjs
+++ adland-vertex/electron/preload.cjs
@@ -2,5 +2,9 @@ const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('vertex',{
   window:{min:()=>ipcRenderer.invoke('window','min'),max:()=>ipcRenderer.invoke('window','max'),close:()=>ipcRenderer.invoke('window','close')},
   openExternal:url=>ipcRenderer.invoke('open-external',url),
-  manifest:()=>ipcRenderer.invoke('manifest'),scanInstalled:names=>ipcRenderer.invoke('scan-installed',names),launchApp:name=>ipcRenderer.invoke('launch-app',name),downloadRelease:(url,name)=>ipcRenderer.invoke('download-release',{url,name})
+  manifest:()=>ipcRenderer.invoke('manifest'),scanInstalled:names=>ipcRenderer.invoke('scan-installed',names),launchApp:name=>ipcRenderer.invoke('launch-app',name),
+  downloadRelease:(url,name)=>ipcRenderer.invoke('download-release',{url,name}),
+  onDownloadProgress:cb=>{const fn=(_,data)=>cb(data);ipcRenderer.on('download-progress',fn);return()=>ipcRenderer.removeListener('download-progress',fn)},
+  onDownloadComplete:cb=>{const fn=(_,data)=>cb(data);ipcRenderer.on('download-complete',fn);return()=>ipcRenderer.removeListener('download-complete',fn)},
+  onDownloadError:cb=>{const fn=(_,data)=>cb(data);ipcRenderer.on('download-error',fn);return()=>ipcRenderer.removeListener('download-error',fn)}
 });
