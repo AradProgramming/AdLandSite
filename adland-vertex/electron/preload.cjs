448dc689d4d4e2a29b3a1570b28b0ac52a5ed7bf
AradProgramming <aradprogramming@gmail.com>
2026-09-24 12:11:58 +0330

    Expose Orbit release downloader

--- adland-vertex/electron/preload.cjs
+++ adland-vertex/electron/preload.cjs
@@ -2,5 +2,5 @@ const {contextBridge,ipcRenderer}=require('electron');
 contextBridge.exposeInMainWorld('vertex',{
   window:{min:()=>ipcRenderer.invoke('window','min'),max:()=>ipcRenderer.invoke('window','max'),close:()=>ipcRenderer.invoke('window','close')},
   openExternal:url=>ipcRenderer.invoke('open-external',url),
-  manifest:()=>ipcRenderer.invoke('manifest'),scanInstalled:names=>ipcRenderer.invoke('scan-installed',names),launchApp:name=>ipcRenderer.invoke('launch-app',name)
+  manifest:()=>ipcRenderer.invoke('manifest'),scanInstalled:names=>ipcRenderer.invoke('scan-installed',names),launchApp:name=>ipcRenderer.invoke('launch-app',name),downloadRelease:(url,name)=>ipcRenderer.invoke('download-release',{url,name})
 });
