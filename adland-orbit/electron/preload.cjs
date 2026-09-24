954dd89971548b783e07599c883bad47c3221190
AradProgramming <aradprogramming@gmail.com>
2026-09-24 13:08:31 +0330

    Expose Orbit app launcher IPC

--- adland-orbit/electron/preload.cjs
+++ adland-orbit/electron/preload.cjs
@@ -1,2 +1,7 @@
 const {contextBridge,ipcRenderer}=require('electron');
-contextBridge.exposeInMainWorld('orbit',{window:a=>ipcRenderer.invoke('window',a),open:u=>ipcRenderer.invoke('open',u),checkUpdates:()=>ipcRenderer.invoke('check-updates')});
+contextBridge.exposeInMainWorld('orbit',{
+  window:a=>ipcRenderer.invoke('window',a),
+  open:u=>ipcRenderer.invoke('open',u),
+  launchApp:name=>ipcRenderer.invoke('launch-app',name),
+  checkUpdates:()=>ipcRenderer.invoke('check-updates')
+});
